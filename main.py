901285edfca40f66098924948cb6784ec8a0e911
Diana <dicomeinit@gmail.com>
2022-09-09 15:30:22 +0300

    If Flight Price Lower than in Google Sheet send an SMS

--- main.py
+++ main.py
@@ -2,26 +2,25 @@
 from datetime import datetime, timedelta
 from data_manager import DataManager
 from flight_search import FlightSearch
+from notification_manager import NotificationManager
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
 flight_search = FlightSearch()
-
+notification_manager = NotificationManager()
 
 ORIGIN_CITY_IATA = "LON"
 
+sheet_data = data_manager.get_destination_data()
 
-#  5. In main.py check if sheet_data contains any values for the "iataCode" key.
-#  If not, then the IATA Codes column is empty in the Google Sheet.
-#  In this case, pass each city name in sheet_data one-by-one
-#  to the FlightSearch class to get the corresponding IATA code
-#  for that city using the Flight Search API.
-#  You should use the code you get back to update the sheet_data dictionary.
+
 if sheet_data[0]["iataCode"] == "":
-    for row in sheet_data:
-        row["iataCode"] = flight_search.get_destination_code(row["city"])
-    data_manager.destination_data = sheet_data
+    city_names = [row["city"] for row in sheet_data]
+    print(city_names)
+    codes = flight_search.get_destination_code()
     data_manager.update_destination_codes()
+    sheet_data = data_manager.get_destination_data()
+
 
 tomorrow = datetime.now() + timedelta(days=1)
 six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
@@ -33,3 +32,9 @@ for destination in sheet_data:
         from_time=tomorrow,
         to_time=six_month_from_today
     )
+
+    if flight.price < destination["lowestPrice"]:
+        notification_manager.send_message(
+            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport}"
+                    f"to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+        )
--- notification_manager.py
+++ notification_manager.py
@@ -1,3 +1,24 @@
+import config
+from twilio.rest import Client
+from flight_data import FlightData
+
+
+AUTH_TOKEN = config.twilio_auth_token
+ACCOUNT_SID = config.twilio_account_sid
+TWILIO_PHONE_NUMBER = config.twilio_phone_num
+MY_PHONE_NUMBER = config.my_phone_num
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def __init__(self):
+        self.client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+    def send_message(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=TWILIO_PHONE_NUMBER,
+            to=MY_PHONE_NUMBER,
+        )
+        print(message.sid)
+
